be0667cf729fbabd01d933c9fa40f953440ff3d8
gkothar1 <gkothar1@binghamton.edu>
2020-06-15 23:25:46 -0400

    add missing files

---
+++ src/riscvsim/evict_policy.h
@@ -0,0 +1,49 @@
+/**
+ * Eviction policy wrapper for set associative data structures
+ *
+ * MARSS-RISCV : Micro-Architectural System Simulator for RISC-V
+ *
+ * Copyright (c) 2020 Gaurav Kothari {gkothar1@binghamton.edu}
+ * State University of New York at Binghamton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef _EVICT_POLICY_H_
+#define _EVICT_POLICY_H_
+
+#include <inttypes.h>
+
+typedef struct EvictPolicy
+{
+    int type;
+    int num_sets;
+    int num_ways;
+
+    /* MRU bit map for all the ways in a set */
+    uint64_t *sets;
+
+    void (*reset)(struct EvictPolicy *p);
+    void (*use)(struct EvictPolicy *p, int set, int way);
+    int (*evict)(struct EvictPolicy *p, int set);
+} EvictPolicy;
+
+EvictPolicy *evict_policy_create(int sets, int ways, int policy_type);
+void evict_policy_free(EvictPolicy **);
+
+#endif /* _EVICT_POLICY_H_ */
---
+++ src/riscvsim/memory_hierarchy.c
@@ -0,0 +1,193 @@
+/**
+ * Memory Hierarchy
+ *
+ * MARSS-RISCV : Micro-Architectural System Simulator for RISC-V
+ *
+ * Copyright (c) 2017-2019 Gaurav Kothari {gkothar1@binghamton.edu}
+ * State University of New York at Binghamton
+ *
+ * Copyright (c) 2018-2019 Parikshit Sarnaik {psarnai1@binghamton.edu}
+ * State University of New York at Binghamton
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dramsim_wrapper_c_connector.h"
+#include "memory_hierarchy.h"
+
+static int
+mem_hierarchy_insn_read(MemoryHierarchy *mem_hierarchy, target_ulong paddr,
+                        int bytes, int stage_id, int priv)
+{
+    if (mem_hierarchy->p->enable_l1_caches)
+    {
+        return mem_hierarchy->icache->read(mem_hierarchy->icache, paddr, bytes,
+                                           (void *)&stage_id, priv);
+    }
+
+    mem_hierarchy->mem_controller->create_mem_request(
+        mem_hierarchy->mem_controller, paddr, bytes, Read, (void *)&stage_id);
+    return 1;
+}
+
+static int
+mem_hierarchy_data_read(MemoryHierarchy *mem_hierarchy, target_ulong paddr,
+                        int bytes, int stage_id, int priv)
+{
+    if (mem_hierarchy->p->enable_l1_caches)
+    {
+        return mem_hierarchy->dcache->read(mem_hierarchy->dcache, paddr, bytes,
+                                           (void *)&stage_id, priv);
+    }
+
+    mem_hierarchy->mem_controller->create_mem_request(
+        mem_hierarchy->mem_controller, paddr, bytes, Read, (void *)&stage_id);
+    return 1;
+}
+
+static int
+mem_hierarchy_data_write(MemoryHierarchy *mem_hierarchy, target_ulong paddr,
+                         int bytes, int stage_id, int priv)
+{
+    if (mem_hierarchy->p->enable_l1_caches)
+    {
+        return mem_hierarchy->dcache->write(mem_hierarchy->dcache, paddr, bytes,
+                                            (void *)&stage_id, priv);
+    }
+
+    mem_hierarchy->mem_controller->create_mem_request(
+        mem_hierarchy->mem_controller, paddr, bytes, Write, (void *)&stage_id);
+    return 1;
+}
+
+static int
+mem_hierarchy_pte_read(MemoryHierarchy *mem_hierarchy, target_ulong paddr,
+                       int bytes, int stage_id, int priv)
+{
+    return mem_hierarchy->mem_controller->create_mem_request_pte(
+        mem_hierarchy->mem_controller, paddr, bytes, Read, (void *)&stage_id);
+}
+
+static int
+mem_hierarchy_pte_write(MemoryHierarchy *mem_hierarchy, target_ulong paddr,
+                        int bytes, int stage_id, int priv)
+{
+    return mem_hierarchy->mem_controller->create_mem_request_pte(
+        mem_hierarchy->mem_controller, paddr, bytes, Write, (void *)&stage_id);
+}
+
+MemoryHierarchy *
+memory_hierarchy_init(const SimParams *p)
+{
+    MemoryHierarchy *mem_hierarchy;
+
+    mem_hierarchy = (MemoryHierarchy *)calloc(1, sizeof(MemoryHierarchy));
+    assert(mem_hierarchy);
+    mem_hierarchy->p = (SimParams *)p;
+
+    /* Setup memory controller */
+    PRINT_INIT_MSG("Setting up memory controller");
+    mem_hierarchy->mem_controller = mem_controller_init(p);
+
+    /* Setup caches */
+    if (p->enable_l1_caches)
+    {
+        mem_hierarchy->cache_line_size
+            = p->words_per_cache_line * sizeof(target_ulong);
+
+        /* If caches are enabled, set burst length to cache line size */
+        mem_hierarchy->mem_controller->set_burst_length(
+            mem_hierarchy->mem_controller, mem_hierarchy->cache_line_size);
+
+        /* If DRAMSim2 memory model is used, its burst length must be equal to
+         * cache line size */
+        if (mem_hierarchy->mem_controller->dram_model_type == MEM_MODEL_DRAMSIM)
+        {
+            assert(mem_hierarchy->mem_controller->burst_length
+                   == dramsim_get_burst_size());
+        }
+
+        if (p->enable_l2_cache)
+        {
+            PRINT_INIT_MSG("Setting up l2-shared cache");
+            mem_hierarchy->l2_cache = cache_init(
+                SharedCache, L2, p->l2_shared_cache_size,
+                mem_hierarchy->cache_line_size, p->l2_shared_cache_ways,
+                p->l2_shared_cache_read_latency,
+                p->l2_shared_cache_write_latency, NULL, p->words_per_cache_line,
+                p->l2_shared_cache_evict,
+                (CacheWritePolicy)p->cache_write_policy,
+                (CacheReadAllocPolicy)p->cache_read_allocate_policy,
+                (CacheWriteAllocPolicy)p->cache_write_allocate_policy,
+                mem_hierarchy->mem_controller);
+        }
+
+        PRINT_INIT_MSG("Setting up l1-instruction cache");
+        mem_hierarchy->icache = cache_init(
+            InstructionCache, L1, p->l1_code_cache_size,
+            mem_hierarchy->cache_line_size, p->l1_code_cache_ways,
+            p->l1_code_cache_read_latency, 1, mem_hierarchy->l2_cache,
+            p->words_per_cache_line, p->l1_code_cache_evict,
+            (CacheWritePolicy)p->cache_write_policy,
+            (CacheReadAllocPolicy)p->cache_read_allocate_policy,
+            (CacheWriteAllocPolicy)p->cache_write_allocate_policy,
+            mem_hierarchy->mem_controller);
+
+        PRINT_INIT_MSG("Setting up l1-data cache");
+        mem_hierarchy->dcache = cache_init(
+            DataCache, L1, p->l1_data_cache_size,
+            mem_hierarchy->cache_line_size, p->l1_data_cache_ways,
+            p->l1_data_cache_read_latency, p->l1_data_cache_write_latency,
+            mem_hierarchy->l2_cache, p->words_per_cache_line,
+            p->l1_data_cache_evict, (CacheWritePolicy)p->cache_write_policy,
+            (CacheReadAllocPolicy)p->cache_read_allocate_policy,
+            (CacheWriteAllocPolicy)p->cache_write_allocate_policy,
+            mem_hierarchy->mem_controller);
+    }
+
+    mem_hierarchy->insn_read_delay = &mem_hierarchy_insn_read;
+    mem_hierarchy->data_read_delay = &mem_hierarchy_data_read;
+    mem_hierarchy->data_write_delay = &mem_hierarchy_data_write;
+    mem_hierarchy->pte_read_delay = &mem_hierarchy_pte_read;
+    mem_hierarchy->pte_write_delay = &mem_hierarchy_pte_write;
+
+    return mem_hierarchy;
+}
+
+void
+memory_hierarchy_free(MemoryHierarchy **mem_hierarchy)
+{
+    if ((*mem_hierarchy)->p->enable_l1_caches)
+    {
+        if ((*mem_hierarchy)->l2_cache)
+        {
+            cache_free(&(*mem_hierarchy)->l2_cache);
+        }
+        cache_free(&(*mem_hierarchy)->dcache);
+        cache_free(&(*mem_hierarchy)->icache);
+    }
+
+    mem_controller_free(&(*mem_hierarchy)->mem_controller);
+
+    free(*mem_hierarchy);
+    *mem_hierarchy = NULL;
+}
